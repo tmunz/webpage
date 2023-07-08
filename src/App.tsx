7933d49a03fd7457e41dffe62087548da5ffec46
Tobias Munzert <tobias.munzert@gmail.com>
2023-07-08 13:09:18 +0200

    base structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,31 @@ import React from 'react';
 
 export function App() {
   return (
-    <div>
-      it works
-    </div>
+    <>
+      Currently work in progress, coming soon
+      <ul>
+        <li>Vita / About</li>
+        <li>Art</li>
+        <li>Photo</li>
+        <li>Dev
+          <ul>
+            <li>Golden Seeds</li>
+            <li>Pebble Alpha Binary</li>
+            <li>Magic Anagram</li>
+            <li>Glassbox (Volley)</li>
+            <li>Concepts (Canvacity)</li>
+            <li>other (inlinegraph)</li>
+          </ul>
+        </li>
+        <li>Lego
+          <ul>
+            <li>Flowers</li>
+            <li>300 SL</li>
+            <li>Ultimate Air- & Spacecraft Collection</li>
+            <li>Project Brickstorm</li>
+          </ul>
+        </li>
+      </ul>
+    </>
   );
 }
