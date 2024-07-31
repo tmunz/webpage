5369003f69dd00b1f3d7a65ad75a2202d3cd195e
Tobias Munzert <tobias.munzert@gmail.com>
2024-07-31 23:21:35 +0200

    style improvements

--- src/app/ui/ImageGallery.tsx
+++ src/app/ui/ImageGallery.tsx
@@ -138,7 +138,7 @@ export function ImageGallery({ sections, desiredMinHeight = 250, gap = 20 }: Ima
         const currentImageIndex = state !== null ? state[1] : 0;
         const imageIndex = currentImageIndex + e;
         if (imageIndex < 0) {
-          const sectionIndex = currentSectionIndex - 1 + sectionsLength % sectionsLength;
+          const sectionIndex = (currentSectionIndex - 1 + sectionsLength) % sectionsLength;
           return [sectionIndex, sections[sectionIndex].data.length - 1];
         } else if (currentSectionLength <= imageIndex) {
           return [(currentSectionIndex + 1) % sectionsLength, 0];
